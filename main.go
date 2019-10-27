17b20576f0fd3d1576e933a1d4881ea885cf3dd7
Gianpaolo Del Matto <delmatto@phunsites.net>
2019-10-27 22:00:10 +0100

    add API prototype

---
+++ main.go
@@ -0,0 +1,32 @@
+/*
+ * Swagger DoH
+ *
+ * This is a DNS-over-HTTP (DoH) resolver written in Go.
+ *
+ * API version: 0.1
+ * Contact: dev@phunsites.net
+ */
+
+package main
+
+import (
+	"log"
+	"net/http"
+
+	// WARNING!
+	// Change this to a fully-qualified import path
+	// once you place this file into your project.
+	// For example,
+	//
+	//    sw "github.com/myname/myrepo/go"
+	//
+	sw "./go"
+)
+
+func main() {
+	log.Printf("Server started")
+
+	router := sw.NewRouter()
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
